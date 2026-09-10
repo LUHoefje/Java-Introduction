e2ebd969a42d36161a58ddb79dfe46cbd782b1cc
LUHoefje <luhoefje@proton.me>
2026-09-10 13:50:38 +0200

    Discovering data types

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        /* JAVA BASIC SYNTAX */
+        /* DATA TYPES & VARIABLES */
+
+        // Hello I am a comment
+        /*
+         * And I am a multiline Comment!
+         * I can come in quite handy for explaining stuff!
+         */
+
+        // Step 1: Read in File
+        // Step 2: Clean Up Input...
 
         /* Variables */
 
@@ -10,12 +19,34 @@ public class Main {
         // OR
         // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
 
-        int points;     // Declaration
-        points = 100;   // Assignment
-        int x = -69;    // Initialization (Declaration + Assignment)
+        /* DATA TYPES */
 
-        // Every instruction/line ends with a semicolon ;
-        // This is like a period at the end of a sentence .
-        // int y = 42
+        // Integers (Store whole numbers (including negative ones))
+        // Go from -2147483648 to 2147483647
+        int points;
+        points = 100;
+        int x = -69;
+
+
+        // Floating Point Numbers (float & double)
+        float pi = 3.14159265f;
+        double e = 2.718281828;
+
+        float accountValue = 20.20f; // NEVER DO THIS!
+
+        // For real numbers, however! Precision can be an issue!
+        // Scientific notation: 314159265 * 10^-8
+
+
+        // Booleans (True or False)
+        boolean likedVideo = true;
+        boolean subscribed = false;
+
+
+        // Chars & Strings
+        char grade = 'A';
+        String name = "Kaupenjoe likes this video";
+
+
     }
 }
