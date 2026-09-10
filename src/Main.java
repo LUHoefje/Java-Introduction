4f01dc2a51ca2d4e067185c246f1b747c1bc0ca0
LUHoefje <luhoefje@proton.me>
2026-09-10 16:34:44 +0200

    Discovering the capabilities of java with math

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,58 @@
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        /* INPUT & OUTPUT */
+        /* INTEGERS & MATH */
 
-        /* OUTPUT */
-        System.out.println("Hello world! This is my first output");
+        int a = 100;
+        int b = 200;
 
-        // values - Printing values
-        int life = 42;
-        System.out.println(life);
+        /* ADDITION */
+        int sum = a + b;
+        System.out.println("Sum:" + sum);
 
-        // value and string - string concetation
-        int grade = 85;
-        System.out.println("your grade is " + grade);
+        sum = 50;
+        System.out.println("Sum:" + sum); // different answer as we changed the variable sum
 
 
-        /* INPUT */
-        System.out.println("Input a number"); // asks for a number in the CLI
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt(); // int requires it to be a number
-        System.out.println("You just typed " + number);
+        /* SUBTRACTION */
+        int sub = a - b;
+        System.out.println("Subtraction: " + sub);
 
-        String input = scanner.next();
-        System.out.println("You just typed (string): " + input);
+        /* MULTIPLICATION */
+        int multi = a * b;
+        System.out.println("Multiplication: " + multi);
+
+        /* DIVISION */
+        int division = a / b;
+        System.out.println("Division: " + division); // will always display 0
+
+        double x = 100;
+        double y = 200;
+        double doubleDivision = x / y;
+        System.out.println("Division: " + doubleDivision); // this will not display 0
+
+        // No division by 0!
+
+        /* REMAINDER (MODULO) */
+        int remainder = 100 % 2; // = 0 means it is an even number | = 1 means it is an uneven number
+        System.out.println("Remainder of 100 / 2: " + remainder);
+
+        remainder = 100 % 3;
+        System.out.println("Remainder of 100 / 3 : " + remainder);
+
+        /* MATH METHODS */
+        System.out.println("Rounding: " + Math.round(6.4));         // Output: 6
+        System.out.println("Rounding down: " + Math.floor(6.4));    // Output: 6
+        System.out.println("Rounding up: " + Math.ceil(6.4));       // Output: 7
+        System.out.println("Exponent : " + Math.pow(2, 3));         // Output: 2^3 = 8
+
+        // More "Math Methods"
+        // Math.
+
+
+        // "RANDOM" Numbers
+        System.out.println("Random number between 0 - 10: " + Math.round(Math.random() * 10));
+        System.out.println("Random number between 0 - 10: " + Math.random() * 10);
+        System.out.println("Random number between 0 - 20: " + Math.round(Math.random() * 20));
 
     }
 }
