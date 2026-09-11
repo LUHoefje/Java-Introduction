dd8e4010ab7862635dedca03e6c6134f4b2631a7
Lukas Hoefnagel <luhoefje@proton.me>
2026-09-11 13:57:17 +0200

    Discovering more String methods

--- src/Main.java
+++ src/Main.java
@@ -2,61 +2,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /* STRING & STRING METHODS */
 
-        /* IF & SWITCH */
+        String name = "LUHoefje";
+        String occupation = "Apprentice";
+        occupation += "r";
+
+        System.out.println("My name is " + name + " my occupation is " + occupation);
+
+        String sentence = "Hello, how are you doing? Hello";
+        System.out.println(sentence);
+
+        // Upper case & Lower case
+        System.out.println("Lower case: " + sentence.toLowerCase()); // All lower case
+        System.out.println("Upper case: " + sentence.toUpperCase()); // All upper case
+
+        // Contain
+        System.out.println("Does it contain \"hello\": " + sentence.contains("hello"));
+
+        // Replacing
+        System.out.println(sentence.replace("Hello", "Howdy, Howdy"));
+
+        // charAt
+        System.out.println(sentence.charAt(8));
+
+        System.out.println(sentence);
+
+
+        System.out.println("---------------------------------------------------------");
+
+        // Think back String Comparisons!
+        System.out.println("What is your name");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How much health you have?");
-        int health = scanner.nextInt();
-        boolean isAlive = health > 0;
-
-        // check if you are alive with the given argument
-        if(isAlive) {
-            System.out.println("You are still alive");
-        } else {
-            System.out.println("You are not alive");
-        }
-
-
-        // checks if you passed with honors or just passed with the given argument
-        System.out.println("How many points did you have in the exam?");
-        int points = scanner.nextInt();
-
-        if(points >= 100) {
-            System.out.println("Passed with honors");
-        } else if(points >= 50) {
-            System.out.println("You passed");
-        } else {
-            System.out.println("You failed");
-        }
-
-
-        /* SWITCH */
-
-        System.out.println("Give me an number between 0 and 5");
-        int studentId = scanner.nextInt();
-        switch (studentId) {
-            case 0:
-                System.out.println("Student Sarah has id 0");
-                break;
-            case 1:
-                System.out.println("Student Mara has id 1");
-                break;
-            case 2:
-                System.out.println("Student Richard has id 2");
-                break;
-            case 3:
-                System.out.println("Student John has id 3");
-                break;
-            case 4:
-                System.out.println("Student Ludwig has id 4");
-                break;
-            case 5:
-                System.out.println("Student Nano id 5");
-                break;
-            default:
-                System.out.println("ERROR: Student with id " + studentId + " not found!");
-    }
-
 
 
     }
