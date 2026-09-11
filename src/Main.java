b3ba8fcf8d3ef511bd5f2585a23bf562b8634ab4
Lukas Hoefnagel <luhoefje@proton.me>
2026-09-11 10:34:23 +0200

    Explored booleans a bit more

--- src/Main.java
+++ src/Main.java
@@ -30,9 +30,42 @@ public class Main {
         // BEWARE of STRINGS
         Scanner scanner = new Scanner(System.in);
         String name = scanner.next();
-        boolean isLUHoefje = name == "LUHoefje"
+        boolean isLUHoefje = name.equals("LUHoefje"); // .equals for strings
+
+        System.out.println("Is your name LUHoefje? " + isLUHoefje);
 
 
+        /* LOGIC */
+        // Addition and "subtraction" for booleans
 
+        // (BOOLEAN logic BOOLEAN) ==> BOOLEAN
+
+        boolean passTest1 = true;
+        boolean passTest2 = true;
+        boolean hasHonors = passTest1 && passTest2;
+
+        boolean hasPickaxe = true;
+        int durability = 5;
+        int neededDurability = 9;
+
+        boolean canMine3x3Area = (hasPickaxe && (durability >= neededDurability)) || isCreative;
+
+        System.out.println("You can mine a 3x3 area: " + canMine3x3Area);
+
+        /* DRINKING AGE EXAMPLE */
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+        System.out.println("Put in true or false, are you from Europe?");
+        boolean isEuropean = scanner.nextBoolean();
+
+        int euDrinkingAge = 18;
+        int usDrinkingAge = 21;
+
+        boolean isAmerican = !isEuropean;
+        boolean couldDrinkInEu = age >= euDrinkingAge;
+        boolean couldDrinkInUs = age >= usDrinkingAge;
+
+        boolean canPersonDrinkAlcohol = (isEuropean && couldDrinkInEu) || (isAmerican && couldDrinkInUs);
+        System.out.println("Can you drink? " + canPersonDrinkAlcohol);
     }
 }
