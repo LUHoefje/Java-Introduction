736d92a3a932137e01183b882ecaf94f9726a058
Lukas Hoefnagel <luhoefje@proton.me>
2026-09-11 10:08:26 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,9 @@
-import java.util.Scanner;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        /* BOOLEANS & LOGIC */
-
-        // Booleans are either true or false
-        boolean isSurvival = true;
-        System.out.println("Player is in Survival: " + isSurvival);
-        boolean isAdventure = false;
-        System.out.println("Player is in Adventure: " + isAdventure);
-
-        boolean isCreative = false;
-        System.out.println("Player is in creative: " + isCreative);
-
-
-        /* COMPARISON (< <= >= > == !=) */
-        int points = 80;
-        int pointsForPassing = 55;
-        boolean hasPassedTest = points >= pointsForPassing;
-        System.out.println("Passed: "+ hasPassedTest);
-
-        int health = 10;
-        boolean isAlive = health > 0;
-        System.out.println("Player is alive: " + isAlive);
-
-        // VARIABLE/VALUE comparison VARIABlE/VALUE
-        //int + int ==> int
-
-        // BEWARE of STRINGS
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.next();
-        boolean isLUHoefje = name == "LUHoefje"
-
-
-
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
     }
 }
